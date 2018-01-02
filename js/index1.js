249c4b22cd8ea340c7abe3549156c92461144728
LoneMarianne <loneoesterlund@gmail.com>
2018-01-02 15:40:51 +0100

    mac adresse ændret

--- js/index1.js
+++ js/index1.js
@@ -1,5 +1,6 @@
 //Adressen på Bluetooth Mate, Password er 1234
-var macAddress = "00:06:66:07:B1:B7";
+var macAddress = "00:15:83:00:41:D0";
+";
 
 function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
